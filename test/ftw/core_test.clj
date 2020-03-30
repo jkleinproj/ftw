9191f3603d9a6a9d9cd0f985fbaa22760cd32f27
michaelbakshandeh <mikebday99@gmail.com>
2020-03-30 17:56:41 -0400

    new files

--- test/ftw/core_test.clj
+++ test/ftw/core_test.clj
@@ -1,6 +1,11 @@
 (ns ftw.core-test
   (:require [clojure.test :refer :all]
-            [ftw.core :refer :all]))
+            [ftw.core :refer :all]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io]))
+
+(defn main- [args]
+  (csv/read-csv "test"))
 
 (deftest a-test
   (testing "FIXME, I fail."
