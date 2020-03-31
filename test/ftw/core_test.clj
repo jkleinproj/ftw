15afb6ce5464083b36342ec2d1c821b7c5f27952
michaelbakshandeh <mikebday99@gmail.com>
2020-03-31 12:21:15 -0400

    read in csv file from my drive

--- test/ftw/core_test.clj
+++ test/ftw/core_test.clj
@@ -7,6 +7,27 @@
 (defn main- [args]
   (csv/read-csv "test"))
 
+; reads in the csv file
+(with-open [reader (io/reader "/Users/michaelbakshandeh/Downloads/stats230/projectdata/mygolfdata.csv")]
+  (doall
+    (csv/read-csv reader)))
+
+; converts csv file to traditional dictionary structure
+(defn csv-data->maps [csv-data]
+  (map zipmap
+       (->> (first csv-data)
+            (map keyword)
+            repeat)
+       (rest csv-data)))
+
+; runs the traditional dictionary structure on csv-data
+(csv-data->maps (with-open [reader (io/reader "/Users/michaelbakshandeh/Downloads/stats230/projectdata/mygolfdata.csv")]
+                  (doall
+                    (csv/read-csv reader))))
+
+; want to figure out how to get rid of the ': (row number)' column
+; any ideas of how to do this in clojure??
+
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
